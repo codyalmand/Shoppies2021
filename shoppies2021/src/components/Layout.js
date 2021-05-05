5051c073c8f22da9b1c71f03ae03ba50c5a740f7
codyalmand <codybalmand@gmail.com>
2021-05-05 15:13:44 -0500

    Added component files, layout containers

---
+++ shoppies2021/src/components/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const style = {
+    containerLeft: {
+        backgroundColor: 'green',
+        display: 'grid',
+    },
+    containerRight: {
+        display: 'grid',
+        backgroundColor: 'blue',
+    }
+}
+
+const Layout = () => {
+    return (
+        <main className = "row">
+            <div className = "col-6 containerLeft" style={style.containerLeft}>hello</div>
+            <div className = "col-6 containerRight" style={style.containerRight}>hello</div>
+        </main>
+    )
+}
+
+export default Layout;
