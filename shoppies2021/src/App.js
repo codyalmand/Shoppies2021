221058cd18dc9edf1d9500fa5feabb7a0bb81aa6
codyalmand <codybalmand@gmail.com>
2021-05-07 19:19:15 -0500

    Removed Card component from App file

--- shoppies2021/src/App.js
+++ shoppies2021/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Layout from './components/Layout'
-import Cards from './components/Cards'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Layout/>
-      <Cards/>
     </div>
   );
 }
